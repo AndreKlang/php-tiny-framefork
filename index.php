44f2996685d3a7045c788ab0cdad2bb9ea813e40
AndreKlang <andre.klang@evalent.com>
2016-03-06 14:42:40 +0100

    Fixed namespace issue, deleted demo-stuff

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
 require_once "vendor/autoload.php";
 
-use Klang\Tiny\App\Controller;
-use Klang\Tiny\App\Controller\NoRoute;
+use KlangTiny\App\Controller;
+use KlangTiny\App\Controller\NoRoute;
 
 $app = new App();
 
-App::registerController("/user",new Controller\User());
 App::registerNoRouteController(new NoRoute());
 
 App::run();
