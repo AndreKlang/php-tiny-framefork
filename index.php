674e50882e9b6c24114f06688a68ea4071848e8a
AndreKlang <andre.klang@evalent.com>
2016-03-13 17:56:39 +0100

    Updated composer json & index.php

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
 <?php
+
+/**
+ * Example index.php for your app
+ */
+
+// set a timezone
+date_default_timezone_set("Europe/Stockholm");
+
+// fire up the autoloader
 require_once "vendor/autoload.php";
 
-use KlangTiny\App\Controller;
+use KlangTiny\App;
 use KlangTiny\App\Controller\NoRoute;
 
 $app = new App();
 
+// register a controller to use as fallback (404)
 App::registerNoRouteController(new NoRoute());
 
+// Add your own controllers to use
+// Just extend KlangTiny\App\Controller
+// App::registerController(new \Your\Own\Controller\Demo());
+
+// RUN!
 App::run();
